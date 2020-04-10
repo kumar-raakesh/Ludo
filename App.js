ed146df541d7e95dcaaa73e9af2975deb76432be
rakesh <rakesh@leaguex.com>
2020-04-10 15:47:28 +0530

    changes done

--- App.js
+++ App.js
@@ -1,37 +1,7 @@
-
-// import React from 'react';
-// import { StyleSheet, View, Platform } from 'react-native';
-// import { MainPage } from './src/components/MainPage/MainPage';
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <MainPage />
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     marginTop: Platform.OS === 'ios' ? 34 : 0
-//   }
-// });
-
-
-/* eslint-disable react-native/no-inline-styles */
-/* eslint-disable dot-notation */
-/* eslint-disable prettier/prettier */
-
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-// import Board from './src/Board';
-import Board from "./src/components/MainPage/Board"
+// import Board from "./src/components/MainPage/Board"
+import Board from "./src/components/Board"
 
 const App = () => {
   return (
